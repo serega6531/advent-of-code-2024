79ef86ebd3188ebddf9fd01f3990be65b6930c87
Sergey Shkurov <seryoga6531@gmail.com>
2024-12-02 01:41:02 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,38 @@
+import java.util.PriorityQueue
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val queueLeft = PriorityQueue<Int>()
+        val queueRight = PriorityQueue<Int>()
+
+        input.map { line -> line.split("   ") }
+            .map { (a, b) -> a.toInt() to b.toInt() }
+            .map { (a, b) ->
+                queueLeft.add(a)
+                queueRight.add(b)
+            }
+
+        return queueLeft.indices.sumOf { _ ->
+            val left = queueLeft.poll()
+            val right = queueRight.poll()
+            abs(left - right)
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val rightCounts = input.map { line -> line.split("   ") }
+            .map { (_, b) -> b.toInt() }
+            .groupingBy { it }
+            .eachCount()
+
+        return input.map { line -> line.split("   ") }
+            .map { (a, _) -> a.toInt() }
+            .sumOf { a -> a * rightCounts.getOrDefault(a, 0) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
