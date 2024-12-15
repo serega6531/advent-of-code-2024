5e802ec6d20f8d891e50292510243f77de1f9ca8
Sergey Shkurov <seryoga6531@gmail.com>
2024-12-15 22:03:42 +0100

    Day 15 Part 2

--- src/Day15.kt
+++ src/Day15.kt
@@ -1,13 +1,5 @@
 fun main() {
 
-    fun parseMap(input: String): Map {
-        val objects = input.lines().map { line ->
-            line.mapTo(mutableListOf()) { MapObject.parse(it) }
-        }
-
-        return Map(objects)
-    }
-
     fun charToDirection(c: Char): Pair<Int, Int> {
         return when (c) {
             '^' -> Pair(-1, 0)
@@ -24,6 +16,14 @@ fun main() {
     }
 
     fun part1(input: String): Int {
+        fun parseMap(input: String): Map {
+            val objects = input.lines().map { line ->
+                line.mapTo(mutableListOf()) { MapObject.parse(it) }
+            }
+
+            return Map(objects)
+        }
+
         val (mapString, movementsString) = input.split("\n\n")
 
         val map = parseMap(mapString)
@@ -31,7 +31,7 @@ fun main() {
 
         movements.forEach { direction ->
             val player = map.findPlayer()
-            map.move(player.y, player.x, direction)
+            map.tryMove(player.y, player.x, direction)
         }
 
         return map.findBoxes()
@@ -39,7 +39,26 @@ fun main() {
     }
 
     fun part2(input: String): Int {
-       TODO()
+        fun parseMap(input: String): Map {
+            val objects = input.lines().map { line ->
+                line.flatMapTo(mutableListOf()) { MapObject.parse(it).widen() }
+            }
+
+            return Map(objects)
+        }
+
+        val (mapString, movementsString) = input.split("\n\n")
+
+        val map = parseMap(mapString)
+        val movements = parseMovements(movementsString)
+
+        movements.forEach { direction ->
+            val player = map.findPlayer()
+            map.tryMove(player.y, player.x, direction)
+        }
+
+        return map.findBoxes()
+            .sumOf { (y, x) -> y * 100 + x }
     }
 
     val testInput = readEntireInput("Day15_test")
@@ -51,7 +70,7 @@ fun main() {
     part1(input).println()
 
     check(part2(testInput2) == 9021)
-    part2(input)
+    part2(input).println()
 }
 
 private class Map(
@@ -79,14 +98,43 @@ private class Map(
     fun findBoxes(): List<YX> = buildList {
         data.forEachIndexed { y, row ->
             row.forEachIndexed { x, col ->
-                if (col == MapObject.BOX) {
+                if (col == MapObject.BOX || col == MapObject.LARGE_BOX_LEFT) {
                     add(YX(y, x))
                 }
             }
         }
     }
 
-    fun move(y: Int, x: Int, direction: Pair<Int, Int>) {
+    private fun canBeMoved(y: Int, x: Int, direction: Pair<Int, Int>): Boolean {
+        val obj = data[y][x]
+        if (obj == MapObject.WALL || obj == MapObject.EMPTY) {
+            return false
+        }
+
+        val nextY = y + direction.first
+        val nextX = x + direction.second
+
+        if (nextY !in 0..maxY || nextX !in 0..maxX) {
+            return false
+        }
+
+        return data[nextY][nextX] == MapObject.EMPTY || canBeMovedWide(nextY, nextX, direction)
+    }
+
+    private fun canBeMovedWide(y: Int, x: Int, direction: Pair<Int, Int>): Boolean {
+        if (direction.second != 0) { // horizontal movement
+            return canBeMoved(y, x, direction)
+        }
+
+        return when (data[y][x]) {
+            MapObject.WALL, MapObject.EMPTY -> false
+            MapObject.LARGE_BOX_LEFT -> canBeMoved(y, x, direction) && canBeMoved(y, x + 1, direction)
+            MapObject.LARGE_BOX_RIGHT -> canBeMoved(y, x, direction) && canBeMoved(y, x - 1, direction)
+            else -> canBeMoved(y, x, direction)
+        }
+    }
+
+    private fun move(y: Int, x: Int, direction: Pair<Int, Int>) {
         val obj = data[y][x]
         if (obj == MapObject.WALL || obj == MapObject.EMPTY) {
             return
@@ -99,10 +147,37 @@ private class Map(
             return
         }
 
-        move(nextY, nextX, direction)
+        moveWide(nextY, nextX, direction)
         if (data[nextY][nextX] == MapObject.EMPTY) {
             data[nextY][nextX] = obj
             data[y][x] = MapObject.EMPTY
+        }
+    }
+
+    private fun moveWide(y: Int, x: Int, direction: Pair<Int, Int>) {
+        if (direction.second != 0) { // horizontal movement
+            return move(y, x, direction)
+        }
+
+        when (data[y][x]) {
+            MapObject.WALL, MapObject.EMPTY -> return
+            MapObject.LARGE_BOX_LEFT -> {
+                move(y, x, direction)
+                move(y, x + 1, direction)
+            }
+
+            MapObject.LARGE_BOX_RIGHT -> {
+                move(y, x, direction)
+                move(y, x - 1, direction)
+            }
+
+            else -> move(y, x, direction)
+        }
+    }
+
+    fun tryMove(y: Int, x: Int, direction: Pair<Int, Int>) {
+        if (canBeMovedWide(y, x, direction)) {
+            moveWide(y, x, direction)
         }
     }
 
@@ -113,14 +188,24 @@ private class Map(
 }
 
 private enum class MapObject {
-    EMPTY, PLAYER, BOX, WALL;
+    EMPTY, PLAYER, BOX, LARGE_BOX_LEFT, LARGE_BOX_RIGHT, WALL;
 
     fun asChar(): Char {
         return when (this) {
             EMPTY -> '.'
             PLAYER -> '@'
             BOX -> 'O'
+            LARGE_BOX_LEFT -> '['
+            LARGE_BOX_RIGHT -> ']'
             WALL -> '#'
+        }
+    }
+
+    fun widen(): List<MapObject> {
+        return when (this) {
+            BOX -> listOf(LARGE_BOX_LEFT, LARGE_BOX_RIGHT)
+            PLAYER -> listOf(PLAYER, EMPTY)
+            else -> listOf(this, this)
         }
     }
 
