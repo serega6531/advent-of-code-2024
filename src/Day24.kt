628e3963d210e09f082d9de7820cf5206d79ffb8
Sergey Shkurov <seryoga6531@gmail.com>
2024-12-24 14:56:47 +0100

    Day 24 Part 2 (WIP)

--- src/Day24.kt
+++ src/Day24.kt
@@ -47,12 +47,16 @@ fun main() {
         return result
     }
 
-    fun collectOutput(results: Map<Int, Boolean>): Long {
-        val asString = buildString {
+    fun resultAsString(results: Map<Int, Boolean>): String {
+        return buildString {
             results.keys.sortedDescending().forEach {
                 append(if (results.getValue(it)) 1 else 0)
             }
         }
+    }
+
+    fun collectOutput(results: Map<Int, Boolean>): Long {
+        val asString = resultAsString(results)
 
         return asString.toLong(2)
     }
@@ -71,18 +75,54 @@ fun main() {
     }
 
     fun part2(input: String): String {
+        fun createStartingWires(prefix: String, value: String): Map<String, Boolean> {
+            return value.mapIndexed { index, c ->
+                (prefix + index.toString().padStart(2, '0')) to (c == '1')
+            }.toMap()
+        }
+
+        val (_, gates) = parseInput(input)
+        val gateByOutput = gates.associateBy { gate -> gate.resultWire }
+        val parser = AdderParser(gateByOutput)
+
+        fun findUsedGates(wire: String): Set<GateDescription> {
+            val gate = gateByOutput[wire] ?: return emptySet()
+            return findUsedGates(gate.leftWire) + findUsedGates(gate.rightWire) + gate
+        }
+
+        fun hasError(wire: String, bit: Int): Boolean {
+            try {
+                val parsed = parser.parseByName(wire)
+                return parsed is AdderSum && parsed.bit == bit
+            } catch (e: AdderParserException) {
+                return false
+            }
+        }
+
+        val number = "1".repeat(45)
+        val wires = (createStartingWires("x", number) + createStartingWires("y", number)).toMutableMap()
+
+        val results = (0..45).associateWith { getOutput("z" + it.toString().padStart(2, '0'), wires, gateByOutput) }
+        val sum = resultAsString(results)
+
+        val used = (0..45).associateWith { findUsedGates("z" + it.toString().padStart(2, '0')) }
+
+
+        val withErrors = (0..45)
+            .filter { hasError("z" + it.toString().padStart(2, '0'), it) }
+
         TODO()
     }
 
     val testInput = readEntireInput("Day24_test")
     val testInput2 = readEntireInput("Day24_test2")
-    check(part1(testInput) == 4L)
-    check(part1(testInput2) == 2024L)
+    //check(part1(testInput) == 4L)
+    //check(part1(testInput2) == 2024L)
 
     val input = readEntireInput("Day24")
-    part1(input).println()
+    //part1(input).println()
 
-    check(part2(testInput) == "TODO")
+    // check(part2(testInput) == "TODO")
     part2(input).println()
 }
 
@@ -101,3 +141,92 @@ private data class GateDescription(
 private enum class GateOperation {
     AND, OR, XOR
 }
+
+private class AdderParser(
+   private val gateByOutput: Map<String, GateDescription>
+) {
+
+    fun parse(gate: GateDescription): AdderPart {
+        val left = parseByName(gate.leftWire)
+        val right = parseByName(gate.rightWire)
+
+        val (first, second) = sortInputs(left, right)
+
+        return when (gate.operation) {
+            GateOperation.AND -> parseAnd(gate, first, second)
+            GateOperation.OR -> parseOr(gate, first, second)
+            GateOperation.XOR -> parseXor(gate, first, second)
+        }
+    }
+
+    fun parseByName(wire: String): AdderPart {
+        return gateByOutput[wire]?.let { parse(it) } ?: parseVariable(wire)
+    }
+
+    private fun parseAnd(gate: GateDescription, first: AdderPart, second: AdderPart): AdderPart {
+        if (first is X && second is Y && first.bit == second.bit) {
+            return HalfAdderCarry(first.bit)
+        }
+
+        if (first is AdderSum && second is HalfAdderCarry && first.bit == second.bit + 1) {
+            return FullAdderSumAndPart(first.bit)
+        }
+
+        if (first is AdderSum && second is FullAdderCarry && first.bit == second.bit + 1) {
+            return FullAdderSumAndPart(first.bit)
+        }
+
+        throw AdderParserException(gate)
+    }
+
+    private fun parseOr(gate: GateDescription, first: AdderPart, second: AdderPart): AdderPart {
+        if (first is FullAdderSumAndPart && second is HalfAdderCarry && first.bit == second.bit) {
+            return FullAdderCarry(first.bit)
+        }
+
+        throw AdderParserException(gate)
+    }
+
+    private fun parseXor(gate: GateDescription, first: AdderPart, second: AdderPart): AdderPart {
+        if (first is X && second is Y && first.bit == second.bit) {
+            return AdderSum(first.bit)
+        }
+
+        if (first is AdderSum && second is HalfAdderCarry && first.bit == second.bit + 1) {
+            return AdderSum(first.bit)
+        }
+
+        if (first is AdderSum && second is FullAdderCarry && first.bit == second.bit + 1) {
+            return AdderSum(first.bit)
+        }
+
+        throw AdderParserException(gate)
+    }
+
+    private fun sortInputs(left: AdderPart, right: AdderPart): List<AdderPart> {
+        return listOf(left, right).sortedBy { it.javaClass.simpleName }
+    }
+
+    private fun parseVariable(name: String): AdderPart {
+        val bit = name.substring(1).toInt()
+        return if (name.startsWith('x')) {
+            X(bit)
+        } else {
+            Y(bit)
+        }
+    }
+
+}
+
+private sealed interface AdderPart {
+    val bit: Int
+}
+
+private data class HalfAdderCarry(override val bit: Int) : AdderPart
+private data class FullAdderCarry(override val bit: Int) : AdderPart
+private data class FullAdderSumAndPart(override val bit: Int) : AdderPart
+private data class AdderSum(override val bit: Int) : AdderPart
+private data class X(override val bit: Int) : AdderPart
+private data class Y(override val bit: Int) : AdderPart
+
+private class AdderParserException(val gate: GateDescription) : RuntimeException()
